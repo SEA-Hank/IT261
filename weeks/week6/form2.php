f13cfc4d7ef2e371053c9dc721814ccd3dc0fcbb
Hank <horicky7@gmail.com>
2021-02-11 00:45:27 -0800

    week6-Stick-form

--- weeks/week6/form2.php
+++ weeks/week6/form2.php
@@ -99,7 +99,7 @@ if ($form_status["isReady"]) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/styles.css">
-    <title>First Form</title>
+    <title>Stick Form</title>
 </head>
 
 <body>
