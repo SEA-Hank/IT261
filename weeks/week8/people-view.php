1069926c135e5ea4b14bce26f167816c9f09c8bc
Hank <horicky7@gmail.com>
2021-02-25 14:46:09 -0800

    week8-peopleView-images

--- weeks/week8/people-view.php
+++ weeks/week8/people-view.php
@@ -16,6 +16,7 @@ $result = mysqli_query($link, $SQL)  or
 
 if (mysqli_num_rows($result) > 0) {
     $row = mysqli_fetch_assoc($result);
+    $id = htmlentities($row["ID"]);
     $firstName = htmlentities($row["firstName"]);
     $lastName = htmlentities($row["lastName"]);
     $email = htmlentities($row["email"]);
@@ -29,17 +30,24 @@ if (mysqli_num_rows($result) > 0) {
 if (!empty($feedback)) : ?>
     <?= $feedback ?>
 <?php else : ?>
-    <h1>Yay!!! We have made it - here we are</h1>
-    <h2>You are on <?= $firstName ?>'s page</h2>
-    <ul>
-        <li><b>First Name:</b><?= $firstName ?></li>
-        <li><b>Last Name:</b><?= $lastName ?></li>
-        <li><b>Occupation:</b><?= $occupation ?></li>
-        <li><b>Email:</b><?= $email ?></li>
-        <li><b>birthDay:</b><?= $birthDay ?></li>
-    </ul>
-    <p><?= $description ?></p>
-    <p><a href="people.php">return to people page</a></p>
+    <div style="width: 850px;margin:0px auto;">
+        <main style="float:left;width:550px;padding:0px 5px">
+            <h1>Yay!!! We have made it - here we are</h1>
+            <h2>You are on <?= $firstName ?>'s page</h2>
+            <ul>
+                <li><b>First Name:</b><?= $firstName ?></li>
+                <li><b>Last Name:</b><?= $lastName ?></li>
+                <li><b>Occupation:</b><?= $occupation ?></li>
+                <li><b>Email:</b><?= $email ?></li>
+                <li><b>birthDay:</b><?= $birthDay ?></li>
+            </ul>
+            <p><?= $description ?></p>
+            <p><a href="people.php">return to people page</a></p>
+        </main>
+        <aside style="float:left;width:250px;padding-top:20px">
+            <img style="width: 250px;" src="./pic/pic_<?= $id ?>.jpg" alt="">
+        </aside>
+    </div>
 <?php endif;
 mysqli_free_result($result);
 mysqli_close($link);
