3742c39260a7fae5400331e124af11d7968e529a
Hank <horicky7@gmail.com>
2021-03-20 22:18:37 -0700

    index

--- final-project/index.php
+++ final-project/index.php
@@ -2,7 +2,19 @@
 include "./includes/config.php";
 include "./includes/header.php";
 ?>
-<div id="login-wrapper" style="height: 600px;">
-    index
+<div id="index-wrapper">
+    <p id="index-title">Welcome to my home page</p>
+    <div id="content-wrapper">
+        <div>
+            <img src="./images/index.jpeg" alt="">
+        </div>
+        <div>
+            <ul>
+                <li><a href="./cities.php">Retired cities</a></li>
+                <li><a href="./contact.php">Contact us</a></li>
+                <li><a href="./index.php">Switch page</a></li>
+            </ul>
+        </div>
+    </div>
 </div>
 <?php include "./includes/footer.php" ?>
